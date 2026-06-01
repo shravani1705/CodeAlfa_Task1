ea401416566118683811dc58c2d851002a9a5162
Shravani Patil <shravani6699patil@gmail.com>
2026-06-01 11:19:30 +0530

    Add files via upload
    Initial upload of Student Grade Tracker project

---
+++ StudentGradeTracker.java
@@ -0,0 +1,48 @@
+package studentgradetracker;
+
+import java.util.Scanner;
+
+public class StudentGradeTracker {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Marks in Subject 1: ");
+        int sub1 = sc.nextInt();
+
+        System.out.print("Enter Marks in Subject 2: ");
+        int sub2 = sc.nextInt();
+
+        System.out.print("Enter Marks in Subject 3: ");
+        int sub3 = sc.nextInt();
+
+        int total = sub1 + sub2 + sub3;
+        double average = total / 3.0;
+
+        String grade;
+
+        if (average >= 90) {
+            grade = "A";
+        } else if (average >= 75) {
+            grade = "B";
+        } else if (average >= 60) {
+            grade = "C";
+        } else if (average >= 40) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("\n----- Student Report -----");
+        System.out.println("Name: " + name);
+        System.out.println("Total Marks: " + total);
+        System.out.println("Average: " + average);
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
